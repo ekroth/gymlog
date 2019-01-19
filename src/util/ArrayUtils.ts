778a29949c8b6b3e22ced8cc70c977178902dfb3
Andrée Ekroth <andree.ekroth@gmail.com>
2019-01-19 21:09:10 +0100

    util/ArrayUtils: readonly

--- src/util/ArrayUtils.ts
+++ src/util/ArrayUtils.ts
@@ -21,16 +21,20 @@ export const arrayInsert = <T>(
   return newArray
 }
 
-export const arrayRemove = <T>(array: AnArray<T>, index: number): T[] => [
-  ...array.slice(0, index),
-  ...array.slice(index + 1)
-]
+export const arrayRemove = <T>(
+  array: AnArray<T>,
+  index: number
+): ReadonlyArray<T> => [...array.slice(0, index), ...array.slice(index + 1)]
 
 export const arrayUpdate = <T>(
   array: AnArray<T>,
   item: T,
   index: number
-): T[] => [...array.slice(0, index), item, ...array.slice(index + 1)]
+): ReadonlyArray<T> => [
+  ...array.slice(0, index),
+  item,
+  ...array.slice(index + 1)
+]
 
 export const arrayUnique = <T>(array: AnArray<T>): ReadonlyArray<T> =>
   array.filter((v, i, a) => a.indexOf(v) === i)
